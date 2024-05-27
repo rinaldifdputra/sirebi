cb03eb8c0d9edc2c7c3951a7d9ed4f840f323f10
Rinaldi <rinaldi.f.d.putra@gmail.com>
2024-05-27 11:40:15 +0700

    initial commit

---
+++ resources/views/home/components/footer.blade.php
@@ -0,0 +1,16 @@
+<div class="copy">
+    <div class="container">
+        <a href="https://www.smarteyeapps.com/">{{ date('Y') }} &copy; All Rights Reserved | Designed and Developed
+            by
+            Smarteyeapps</a>
+
+        <span>
+            <a><i class="fab fa-github"></i></a>
+            <a><i class="fab fa-google-plus-g"></i></a>
+            <a><i class="fab fa-pinterest-p"></i></a>
+            <a><i class="fab fa-twitter"></i></a>
+            <a><i class="fab fa-facebook-f"></i></a>
+        </span>
+    </div>
+
+</div>
